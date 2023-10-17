000d40cf7c48a5ce302766c89f2af7a164679572
Marcello92tp <marcellokgnoctis92@gmail.com>
2023-10-17 11:49:33 +0200

    esercizio 3 svolto

---
+++ esercizio3spr/src/main/java/com/example/esercizio3spr/Esercizio3sprApplication.java
@@ -0,0 +1,13 @@
+package com.example.esercizio3spr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esercizio3sprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Esercizio3sprApplication.class, args);
+	}
+
+}
---
+++ esercizio3spr/src/test/java/com/example/esercizio3spr/Esercizio3sprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esercizio3spr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Esercizio3sprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
