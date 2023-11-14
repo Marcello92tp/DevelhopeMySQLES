9be6cc98f791b988efc508fbdc58b799fefc516a
Marcello Gerardi <134273546+Marcello92tp@users.noreply.github.com>
2023-11-14 17:45:57 +0100

    Delete esercizio-5-Spring directory

--- esercizio-5-Spring/src/main/java/com/example/esercizio5/Spring/Dao/IngredientDAO.java
+++
@@ -1,7 +0,0 @@
-package com.example.esercizio5.Spring.Dao;
-
-import com.example.esercizio5.Spring.entity.Ingredient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IngredientDAO extends JpaRepository<Ingredient,Long> {
-}
--- esercizio-5-Spring/src/main/java/com/example/esercizio5/Spring/Dao/MealDAO.java
+++
@@ -1,7 +0,0 @@
-package com.example.esercizio5.Spring.Dao;
-
-import com.example.esercizio5.Spring.entity.Meal;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MealDAO extends JpaRepository<Meal,Long> {
-}
